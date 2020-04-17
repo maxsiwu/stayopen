8bfc36401859a04e10127acf8f853c9496eed169
maxsiwu <max_asky@yahoo.com>
2020-04-17 15:15:27 -0700

    add keys to ionic items

--- src/components/container/Slider.tsx
+++ src/components/container/Slider.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IonList, IonItem, IonLabel, IonListHeader, IonContent, IonText, IonIcon, IonButton, IonBadge } from '@ionic/react'
+import { IonList, IonItem, IonLabel, IonListHeader, IonContent, IonText, IonIcon, IonButton, IonBadge, IonNote } from '@ionic/react'
 import './Slider.css'
 import { globeOutline, closeOutline } from 'ionicons/icons'
 import { store } from '../..'
@@ -13,30 +13,34 @@ function Slider(props: {location: IShop | null}) {
         className = 'show';
     }
     return (
-        <IonContent id="slider" className={className}>
-            <IonListHeader>
-                <IonLabel color="primary"><h2><b>{props.location?.name}</b></h2></IonLabel>
-                <IonButton fill="clear" onClick={() => onClose()}>
-                    <IonIcon color="medium" icon={closeOutline} size="large"></IonIcon>
+        <IonContent id="slider" className={className} key="slider-block">
+            <IonListHeader key="list-header">
+                <IonLabel color="primary" key="item-name"><h2><b>{props.location?.name}</b></h2></IonLabel>
+                <IonButton fill="clear" onClick={() => onClose()} key="close-button">
+                    <IonIcon color="medium" icon={closeOutline} size="large" key="close-button-icon"></IonIcon>
                 </IonButton>
             </IonListHeader>
-            <IonList lines="none">
-                <IonItem>
-                    <IonButton fill="clear" onClick={() => openUrl(props.location?.url)}>
-                        <IonIcon color="medium" icon={globeOutline}></IonIcon>&nbsp;&nbsp;
-                        <IonText color="medium">Visit Website</IonText>
+            <IonList lines="none" key="list">
+                <IonItem key="list-link">
+                    <IonButton fill="clear" onClick={() => openUrl(props.location?.url)} key="list-button">
+                        <IonIcon color="medium" icon={globeOutline} key="link-icon"></IonIcon>&nbsp;&nbsp;
+                        <IonLabel color="medium" key="link-text">Visit Website</IonLabel>
                     </IonButton>
                 </IonItem>
-                <IonItem>
-                    <IonLabel><h3><b>Services</b></h3></IonLabel>
+                <IonItem key="list-service">
+                    <IonLabel key="service-label"><h3><b>Services</b></h3></IonLabel>
+                </IonItem>
+                <IonItem key="list-service-content">
                     {
-                        props.location?.services?.map(service => <IonBadge color="secondary">{service}</IonBadge>)
+                        props.location?.services?.map(service => <IonBadge color="dark" key={'service' + service}>{service}</IonBadge>)
                     }
                 </IonItem>
-                <IonItem>
-                    <IonLabel><h3><b>Category</b></h3></IonLabel>
+                <IonItem key="list-cat">
+                    <IonLabel key="cat-label"><h3><b>Category</b></h3></IonLabel>
+                </IonItem>
+                <IonItem key="list-cat-content">
                     {
-                        props.location?.categories?.map(category => <IonBadge color="light">{category}</IonBadge>)
+                        props.location?.categories?.map(category => <IonBadge color="light" key={'cat' + category}>{category}</IonBadge>)
                     }
                 </IonItem>
             </IonList>
