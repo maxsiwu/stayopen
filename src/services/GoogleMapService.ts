bb68924a5658967aea2276a643c5df2f90819882
maxsiwu <max_asky@yahoo.com>
2020-04-17 23:09:59 -0700

    change default to silver themed google map

--- src/services/GoogleMapService.ts
+++ src/services/GoogleMapService.ts
@@ -19,14 +19,22 @@ class GoogleMapServices {
 
     createGoogleMap = async (googleMapRef: React.RefObject<HTMLDivElement>) => {
         await this.ready;
-        return new window.google.maps.Map(googleMapRef.current, {
+        let map = new window.google.maps.Map(googleMapRef.current, {
             zoom: 13,
+            mapTypeControlOptions: {
+                mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                        'styled_map']
+            },
             center: {
                 lat: 49.2785508,
                 lng: -123.1415198,
             },
             disableDefaultUI: true,
         });
+        let styledMapType = new window.google.maps.StyledMapType(this.silverMapStyle)
+        map.mapTypes.set('styled_map', styledMapType)
+        map.setMapTypeId('styled_map');
+        return map
     }
 
     createMarker = (item: IShop, googleMap: GoogleMap) => {
@@ -40,5 +48,7 @@ class GoogleMapServices {
             },
         })
     }
+
+    silverMapStyle = [{elementType:'geometry',stylers:[{color:'#f5f5f5'}]},{elementType:'labels.icon',stylers:[{visibility:'off'}]},{elementType:'labels.text.fill',stylers:[{color:'#616161'}]},{elementType:'labels.text.stroke',stylers:[{color:'#f5f5f5'}]},{featureType:'administrative.land_parcel',elementType:'labels.text.fill',stylers:[{color:'#bdbdbd'}]},{featureType:'poi',elementType:'geometry',stylers:[{color:'#eeeeee'}]},{featureType:'poi',elementType:'labels.text.fill',stylers:[{color:'#757575'}]},{featureType:'poi.park',elementType:'geometry',stylers:[{color:'#e5e5e5'}]},{featureType:'poi.park',elementType:'labels.text.fill',stylers:[{color:'#9e9e9e'}]},{featureType:'road',elementType:'geometry',stylers:[{color:'#ffffff'}]},{featureType:'road.arterial',elementType:'labels.text.fill',stylers:[{color:'#757575'}]},{featureType:'road.highway',elementType:'geometry',stylers:[{color:'#dadada'}]},{featureType:'road.highway',elementType:'labels.text.fill',stylers:[{color:'#616161'}]},{featureType:'road.local',elementType:'labels.text.fill',stylers:[{color:'#9e9e9e'}]},{featureType:'transit.line',elementType:'geometry',stylers:[{color:'#e5e5e5'}]},{featureType:'transit.station',elementType:'geometry',stylers:[{color:'#eeeeee'}]},{featureType:'water',elementType:'geometry',stylers:[{color:'#c9c9c9'}]},{featureType:'water',elementType:'labels.text.fill',stylers:[{color:'#9e9e9e'}]}]
 }
 export default new GoogleMapServices();
