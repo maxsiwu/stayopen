c19f061dbe9d159f6d84c9ba9a4b427762adbe63
maxsiwu <max_asky@yahoo.com>
2020-05-10 13:52:20 -0700

    Code Linting

--- src/components/container/ActiveLocationContainer.ts
+++ src/components/container/ActiveLocationContainer.ts
@@ -1,21 +1,19 @@
-import { ILocation } from '../location';
-import Location from '../location'
-import { showLocation, hideLocation } from '../actions'
-import { connect } from 'react-redux'
+import { ILinkSingle, ILinkDispatchSingle } from './../location';
+import Location, { ILocation } from '../location';
+import { showLocation, hideLocation } from '../actions';
+import { connect } from 'react-redux';
 import { RootState } from '../reducers/reducer';
 import { Dispatch } from 'redux';
 
-
-const mapStateToProps = (state: RootState) => ({
+const mapStateToProps = (state: RootState): ILinkSingle => ({
   location: state.location,
   isSliderOpen: state.isSliderOpen
-})
+});
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  showLocation (location: ILocation) {dispatch(showLocation(location))},
-  hideLocation () {dispatch(hideLocation())},
-})
-const ActiveLocationContainer = connect(mapStateToProps, mapDispatchToProps)(Location)
+const mapDispatchToProps = (dispatch: Dispatch): ILinkDispatchSingle => ({
+  showLocation (location: ILocation): void { dispatch(showLocation(location)); },
+  hideLocation (): void { dispatch(hideLocation()); }
+});
+const ActiveLocationContainer = connect(mapStateToProps, mapDispatchToProps)(Location);
 
-
-export default ActiveLocationContainer
+export default ActiveLocationContainer;
--- src/components/reducers/reducer.ts
+++ src/components/reducers/reducer.ts
@@ -1,54 +1,56 @@
+import { ActionTypes } from './../actions';
 import { ILocation } from '../location';
 import { combineReducers } from 'redux';
-import { SHOW_LOCATION, SHOW_ALL, HIDE_LOCATION, SHOW_FILTERED } from './../actions';
 
-const locations = (state:ILocation[] =[], action: {type: string; locations:ILocation[];}) => {
-    switch(action.type){
-        case SHOW_ALL:
-            return action.locations;
-        default:
-            return state;
-    }
-}
+const locations = (state: ILocation[] = [], action: {type: string; locations: ILocation[] }): ILocation[] => {
+  switch (action.type) {
+    case ActionTypes.SHOW_ALL:
+      return action.locations;
+    default:
+      return state;
+  }
+};
 
-const filteredLocations = (state:ILocation[] =[], action: {type: string; locations:ILocation[]; keyword?: string}) => {
-    switch(action.type){
-        case SHOW_FILTERED:
-            let keyword = action.keyword ? action.keyword.toLowerCase() : '';
-            return action.locations.filter(s => s.name.toLowerCase().includes(keyword));
-        default:
-            return state;
-    }
-}
+const filteredLocations = (state: ILocation[] = [], action: {type: string; locations: ILocation[]; keyword?: string}): ILocation[] => {
+  let keyword = '';
+  switch (action.type) {
+    case ActionTypes.SHOW_FILTERED:
+      keyword = action.keyword ? action.keyword.toLowerCase() : '';
+      return action.locations.filter(s => s.name.toLowerCase().includes(keyword));
+    default:
+      return state;
+  }
+};
 
-const location = (state:ILocation|null = null, action: {type: string; location:ILocation|null}) => {
-    switch(action.type){
-        case SHOW_LOCATION:
-            return action.location;
-        case HIDE_LOCATION:
-            return action.location;
-        default:
-            return state;
-    }
-}
+const location = (state: ILocation|null = null, action: {type: string; location: ILocation|null}): ILocation | null => {
+  switch (action.type) {
+    case ActionTypes.SHOW_LOCATION:
+      return action.location;
+    case ActionTypes.HIDE_LOCATION:
+      return action.location;
+    default:
+      return state;
+  }
+};
 
-const isSliderOpen = (state: boolean = false, action: {type: string; isOpen: boolean}) => {
-    switch(action.type){
-        case SHOW_LOCATION:
-            return true;
-        case HIDE_LOCATION:
-            return false;
-        default:
-            return state;
-    }
-}
+const isSliderOpen = (state = false, action: {type: string; isOpen: boolean}): boolean => {
+  switch (action.type) {
+    case ActionTypes.SHOW_LOCATION:
+      return true;
+    case ActionTypes.HIDE_LOCATION:
+      return false;
+    default:
+      return state;
+  }
+};
+
 const locationApp = combineReducers({
-    locations,
-    filteredLocations,
-    location,
-    isSliderOpen
-})
+  locations,
+  filteredLocations,
+  location,
+  isSliderOpen
+});
 
-export default locationApp
+export default locationApp;
 
-export type RootState = ReturnType<typeof locationApp>
+export type RootState = ReturnType<typeof locationApp>;
