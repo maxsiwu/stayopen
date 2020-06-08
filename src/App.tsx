dda8fad5b873a27a376cb5b7e6a836ea6253d7ee
maxsiwu <max_asky@yahoo.com>
2020-06-07 19:29:59 -0700

    try forceRefresh to correct routing on production

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import ErrorPage from './pages/ErrorPage/ErrorPage';
 const App: React.FC = () => {
   return (
     <IonApp>
-      <IonReactRouter>
+      <IonReactRouter forceRefresh={true}>
         <IonHeader>
           <IonToolbar color="primary">
             <IonTitle>Stay Open</IonTitle>
