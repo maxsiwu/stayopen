fa3971e313a533e2e0047db11a5582ac217ce6ad
maxsiwu <max_asky@yahoo.com>
2020-06-10 10:56:28 -0700

    add logout

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { IonPage } from '@ionic/react';
 import './ErrorPage.css';
 
-const ErrorPage = (): JSX.Element => <IonPage><h2>404 Not Found</h2></IonPage>;
+const ErrorPage = (): JSX.Element => <IonPage id="error"><h2>404 Not Found</h2></IonPage>;
 
 export default ErrorPage;
